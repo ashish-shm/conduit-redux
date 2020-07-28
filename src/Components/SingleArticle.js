3ad4c5176f54417f9654109f4d24da9b8fc7edc6
Ashish Sharma <ashish.shm09@gmail.com>
2020-07-28 16:32:36 +0530

    Removed errors of singlearticle page

--- src/Components/SingleArticle.js
+++ src/Components/SingleArticle.js
@@ -4,42 +4,58 @@ import Header from "./Header";
 import { singleArticle } from "../store/actions";
 
 class SingleArticle extends Component {
-
   componentDidMount() {
     const slug = this.props.match.params.slug;
     const url = `https://conduit.productionready.io/api/articles/${slug}`;
     this.props.dispatch(singleArticle(url));
   }
   render() {
-    let { title, createdAt, body } = this.props.state.singleArticle;
-    let  {image, username} 
-     = this.props.state.singleArticle;
+    let singleArticle =
+      this.props.state.singleArticle && this.props.state.singleArticle;
+    let { title, createdAt, body } = singleArticle;
+    // let author =  singleArticle.author
 
     return (
       <div>
         <Header />
-        <>
-          <section className="hero-section">
-            <div className="container">
-              <h1 className="hero-description">{title}</h1>
-              <p className="hero-description">{body}</p>
-              <div className="picture_flex">
-                <div>
-                  <img src={image} alt={username} className="authorDp"></img>
-                </div>
+        <div className="singleArticle-section">
+          <section className=" container">
+            <h1 className="hero-description">{title}</h1>
 
-                <ul className="authorTime">
-                  <li>
-                    <h3 className="articleAuthor">Author: {username}</h3>
-                  </li>
-                  <li>
-                    <p>{new Date(createdAt).toDateString()}</p>
-                  </li>
-                </ul>
+            <div className="picture_flex">
+              <div>
+                <img
+                  className="singleArticle-image"
+                  src={singleArticle.author ? singleArticle.author.image : null}
+                  alt={
+                    singleArticle.author ? singleArticle.author.username : null
+                  }
+                  className="authorDp"
+                ></img>
               </div>
+
+              <ul className="authorTime">
+                <li>
+                  <h3 className="singleArticle-articleAuthor">
+                    Author:{" "}
+                    {singleArticle.author
+                      ? singleArticle.author.username
+                      : null}
+                  </h3>
+                </li>
+                <li>
+                  <p className="singleArticle-date">
+                    {new Date(createdAt).toDateString()}
+                  </p>
+                </li>
+              </ul>
+              <div></div>
             </div>
           </section>
-        </>
+        </div>
+        <div className="body-section container">
+          <p className="body-description">{body}</p>
+        </div>
       </div>
     );
   }
